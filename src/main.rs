873a84c2d09cd1756b33f1eed86a9b4e6ef57e52
Łukasz Strzelczyk <lstrzelczyk.business@gmail.com>
2024-12-05 20:58:49 +0100

    zadanie wykonane wersja 1

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use std::{fs::File, io::{BufRead, BufReader}, process::exit};
 
 
+
 fn main() {
     //file opening
-    let path=".txt";
+    let path="input.txt";
     let file = match File::open(path) {
         Ok(file) =>file,
         Err(e)=>{print!("unable to open {} file, err: {}",path,e);
@@ -11,18 +12,25 @@ fn main() {
     };
     //file reading
     let buffer=BufReader::new(file);
-    let mut list1:Vec<String>=Vec::new();
-    let mut list2:Vec<String>=Vec::new();
+    let mut list1:Vec<i32>=Vec::new();
+    let mut list2:Vec<i32>=Vec::new();
     let lines = buffer.lines();
     //splitting to 2 diffrent list
     for line in lines{
         let line = line.unwrap();
         let values:Vec<&str> =line.split("   ").collect();
-        list1.push(String::from(values[0]));
-        list2.push(String::from(values[1]));        
+        list1.push(values[0].parse().unwrap());
+        list2.push(values[1].parse().unwrap());        
+    }
+    //sorting list
+    list1.sort();
+    list2.sort();
+
+    let mut distances:Vec<i32> = Vec::new(); 
+    for idx in 0..list1.len(){
+        distances.push((list1[idx]-list2[idx]).abs())
     }
 
-    println!("list 1: {list1:?}");
-    println!("list 2: {list2:?}");
-
+    let sum:i32 = distances.into_iter().sum();
+    println!("sum of distances: {}",sum)
 }
